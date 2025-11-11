c68cae4335dc0723b77460142cbb4fee0403b171
vladyslavyatsuta <vy2000eng@gmail.com>
2025-11-10 22:56:14 -0500

    updated register request to get rid of fname and last name

--- dreamify.Domain/Requests/RegisterRequest.cs
+++ dreamify.Domain/Requests/RegisterRequest.cs
@@ -2,8 +2,6 @@ namespace dreamify.Domain.Requests;
 
 public record RegisterRequest
 {
-    public required String FirstName { get; init; }
-    public required String LastName{ get; init; }
     public required String Email { get; init; }
     public required String Password { get; init; }
     
